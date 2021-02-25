824483e96b9391a5f766819239213829452a17f1
Antonin Riviere <antonin@immersive-g.com>
2021-02-25 09:56:23 +0100

    :sparkles: Add gsap exo

---
+++ 04_gsap/start/starter-vanilla/js/scripts.js
@@ -0,0 +1,17 @@
+gsap.registerPlugin(ScrollTrigger)
+
+window.addEventListener('DOMContentLoaded', (event) => {
+	console.log('page ready')
+
+	/* gsap.to('.life__content__circle', {
+		scrollTrigger: {
+			trigger: '.life__content__circle',
+			toggleActions: 'restart reverse restart reverse',
+			start: 'top 80%',
+			end: 'bottom 20%',
+			markers: true,
+		},
+		scale: 1,
+		duration: 0.6,
+	}) */
+})
