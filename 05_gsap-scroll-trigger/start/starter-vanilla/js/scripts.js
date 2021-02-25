0529bdc1e751919bd6f5353f40144df9811d3174
Antonin Riviere <antonin@immersive-g.com>
2021-02-25 14:16:02 +0100

    :sparkles: Add gsap scroll animations

--- 05_gsap-scroll-trigger/start/starter-vanilla/js/scripts.js
+++ 05_gsap-scroll-trigger/start/starter-vanilla/js/scripts.js
@@ -24,4 +24,52 @@ window.addEventListener('DOMContentLoaded', (event) => {
 		scale: 1,
 		duration: 0.6,
 	})*/
+
+  gsap.from('.hero__button', {
+    scale: 0,
+    duration: 0.4,
+    ease: 'back.out(1.7)',
+    scrollTrigger: {
+      trigger: '.hero__button',
+      start: 'top 100%',
+      end: 'bottom 20%',
+      markers: false,
+      toggleActions: 'restart reverse restart reverse'
+    }
+  })
+
+  gsap.to('.life__content__circle', {
+    scale: 2,
+    duration: 0.4,
+    ease: 'back.out(1.7)',
+    scrollTrigger: {
+      trigger: '.life__content__circle',
+      start: 'top 80%',
+      end: 'bottom 20%',
+      markers: false,
+      toggleActions: 'restart reverse restart reverse'
+    }
+  })
+
+  gsap.from(['.life__text'], {
+    opacity: 0,
+    y: 50,
+    duration: 0.6,
+    scrollTrigger: {
+      trigger: '.life__text',
+      start: 'top 90%',
+      end: 'bottom 10%'
+    }
+  })
+
+  gsap.from('.life__title', {
+    opacity: 0,
+    y: 50,
+    duration: 0.6,
+    scrollTrigger: {
+      trigger: '.life__title',
+      start: 'top 90%',
+      end: 'bottom 10%'
+    }
+  })
 })
